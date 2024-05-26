befcaeaa2eccabda13c3a616b7e27f974fe453fe
bjohas <bjohas+git@gmail.com>
2024-05-26 11:30:31 +0100

    Update to test1.sh

--- test1.sh
+++ test1.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -x
 
-# tool="openalex-cli"
-# echo "------------------------ ${tool} -----------------------------------------------------"
-# $tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
-# $tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
-# $tool search --count --title-abs test_quote_1...
-# $tool search --count --title-abs test_quote_2...
+tool="openalex-cli"
+echo "------------------------ ${tool} -----------------------------------------------------"
+$tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
+$tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
+$tool search --count --title-abs test_quote_1...
+$tool search --count --title-abs test_quote_2...
 
-# tool="scopus-cli"
-# echo "------------------------ ${tool} -----------------------------------------------------"
-# $tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
-# $tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
-# $tool search --count --title-abs test_quote_1...
-# $tool search --count --title-abs test_quote_2...
+tool="scopus-cli"
+echo "------------------------ ${tool} -----------------------------------------------------"
+$tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
+$tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
+$tool search --count --title-abs test_quote_1...
+$tool search --count --title-abs test_quote_2...
 
-# tool="scite-cli"
-# echo "------------------------ ${tool} -----------------------------------------------------"
-# $tool search --count EdTech OR "educational technology" OR "education technology"
-# $tool search --count EdTech OR "\"educational technology\"" OR "\"education technology\""
-# $tool search --count test_quote_1...
-# $tool search --count test_quote_2...
+tool="scite-cli"
+echo "------------------------ ${tool} -----------------------------------------------------"
+$tool search --count EdTech OR "educational technology" OR "education technology"
+$tool search --count EdTech OR "\"educational technology\"" OR "\"education technology\""
+$tool search --count test_quote_1...
+$tool search --count test_quote_2...
 
 tool="scholarly-cli"
 echo "------------------------ ${tool} -----------------------------------------------------"
