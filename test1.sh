7cca763bf420db1edd692df28c22d182abb5710e
bjohas <bjohas+git@gmail.com>
2024-05-26 11:30:02 +0100

    Update to test1.sh

---
+++ test1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+
+# tool="openalex-cli"
+# echo "------------------------ ${tool} -----------------------------------------------------"
+# $tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
+# $tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
+# $tool search --count --title-abs test_quote_1...
+# $tool search --count --title-abs test_quote_2...
+
+# tool="scopus-cli"
+# echo "------------------------ ${tool} -----------------------------------------------------"
+# $tool search --count --title-abs EdTech OR "educational technology" OR "education technology"
+# $tool search --count --title-abs EdTech OR "\"educational technology\"" OR "\"education technology\""
+# $tool search --count --title-abs test_quote_1...
+# $tool search --count --title-abs test_quote_2...
+
+# tool="scite-cli"
+# echo "------------------------ ${tool} -----------------------------------------------------"
+# $tool search --count EdTech OR "educational technology" OR "education technology"
+# $tool search --count EdTech OR "\"educational technology\"" OR "\"education technology\""
+# $tool search --count test_quote_1...
+# $tool search --count test_quote_2...
+
+tool="scholarly-cli"
+echo "------------------------ ${tool} -----------------------------------------------------"
+$tool --count --search "EdTech OR \"educational technology\" OR \"education technology\""
+$tool --count --search test_quote_1...
+$tool --count --search test_quote_2...
